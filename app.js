db2a56d1687f6674ceb909eca927e1394db654f2
MahmudAntor <antor.mahmud96@gmail.com>
2020-10-21 19:17:28 +0600

    Express rest api with mongoose

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('connected correctly to server');
+
+}, (err) => { console.log(err); });
+
 var app = express();
 
 // view engine setup
