7021c4426bdfed055da410745cd12f77c41b92c5
MahmudAntor <antor.mahmud96@gmail.com>
2020-10-25 17:15:03 +0600

    erased keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
+    'secretKey': '',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '994027214426292',
-        clientSecret: 'fc5bd32f13c77d965021107e8171155a'
+        clientId: '',
+        clientSecret: ''
     }
 }
